7905cce61e5fb7184c442c390f5a312b52bd8545
taehona6 <taehona6@gmail.com>
2024-02-02 14:39:02 +0900

    초기설정
    janghi.tistory 에서 세팅 참고하고, 홈컨트롤러랑 index.html만 만들었음

---
+++ src/main/java/com/poke/www/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.poke.www.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home(Model m) {
+		m.addAttribute("msg","한글확인");
+		return "index";
+	}
+}
