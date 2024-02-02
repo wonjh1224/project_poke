4e6f0ce685e96f748923a70bfdb90be0267feb8f
taehona6 <taehona6@gmail.com>
2024-02-02 14:18:38 +0900

    Initial commit

---
+++ src/main/java/com/poke/www/ProjectPokeApplication.java
@@ -0,0 +1,13 @@
+package com.poke.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPokeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poke/www/ProjectPokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poke.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
