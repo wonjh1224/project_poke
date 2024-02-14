0bed4cf0acf060fb0fb72795015b287d7ac7203a
taehona6 <taehona6@gmail.com>
2024-02-14 13:02:19 +0900

    도감 살짝 수정(보유하지 않으면 ???로)

--- src/main/resources/static/js/pokedexList.js
+++ src/main/resources/static/js/pokedexList.js
@@ -15,24 +15,19 @@ async function getPokedexList(){
 spreadList()
 function spreadList(){
     getPokedexList().then(result=>{
-        console.log(result)
         let userPokemonIds = [];
         for(pokemon of result.userPokemons){
             userPokemonIds.push(pokemon.pokemonId)
         }
-        console.log(userPokemonIds)
         let pokedexLine = document.getElementById('pokedexLine')
         pokedexLine.innerHTML = ''
-        
-        if(userPokemonIds.includes("6")){
-            console.log("6666666")
-        }
+
         for(pokemon of result.allPokemons){
             pokedexLine.innerHTML += `
                 <div>
-                    <img src="${pokemon.image}" class="${userPokemonIds.includes(pokemon.pokemonId) ? '' : 'gray'} abc">
-                    <p>${pokemon.name}</p>
-                    <p>${pokemon.flavor}</p>
+                    <img src="${pokemon.image}" class="${userPokemonIds.includes(pokemon.pokemonId) ? 'color' : 'gray'} abc">
+                    <p>${userPokemonIds.includes(pokemon.pokemonId)? pokemon.name : '???'}</p>
+                    <p>${userPokemonIds.includes(pokemon.pokemonId)? pokemon.flavor : ''}</p>
                 </div>
             `
         }
