478f217d387cd1dbbc3ea7b3b8479f402a09c7a2
taehona6 <taehona6@gmail.com>
2024-02-27 10:02:18 +0900

    헤더 조금 수정(깨져서)

--- src/main/resources/static/js/storageItem.js
+++ src/main/resources/static/js/storageItem.js
@@ -1,61 +1,61 @@
-document.addEventListener('click',(e)=>{
-    if(e.target.classList.contains('useBtn')){
+document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('useBtn')) {
         let storageId = e.target.dataset.storageid
         let data = {
-			memberId : memberId,
-			storageId : storageId
-		}
-        
-        useItemInStorage(data).then(result=>{
+            memberId: memberId,
+            storageId: storageId
+        }
+
+        useItemInStorage(data).then(result => {
             pokemonNames = [];
-            for(pokemon of result){
+            for (pokemon of result) {
                 pokemonNames.push(pokemon.name)
             }
-            alert(pokemonNames+" 당첨")
+            alert(pokemonNames + " 당첨")
             spreadItems()
         })
     }
 })
 
-async function useItemInStorage(data){
-    try{
+async function useItemInStorage(data) {
+    try {
         const url = "/storage/use-item"
         const config = {
-            method : "post",
-            headers : {
-                "content-type" : "application/json; charset=utf-8"
+            method: "post",
+            headers: {
+                "content-type": "application/json; charset=utf-8"
             },
-            body : JSON.stringify(data)
+            body: JSON.stringify(data)
         }
-        const resp = await fetch(url,config)
+        const resp = await fetch(url, config)
         const result = await resp.json();
         return result;
-    }catch(error){
+    } catch (error) {
         console.log(error)
     }
 }
 
-function spreadItems(){
-    getItemListFromServer(memberId).then(result=>{
+function spreadItems() {
+    getItemListFromServer(memberId).then(result => {
         let itemBox = document.getElementById('itemBox')
         itemBox.innerHTML = ''
-        if(result.length>0){
-            for(let item of result){
-                itemBox.innerHTML +=  `
+        if (result.length > 0) {
+            for (let item of result) {
+                itemBox.innerHTML += `
                 <p>상품명 : ${item.name}</p>
                 <button data-storageid="${item.storageId}" class="useBtn">사용하기</button>
                 <hr>
                 `
             }
-        }else{
+        } else {
             itemBox.innerHTML = `<p>보유중인 상품이 없습니다.</p>`
         }
     })
 }
 
-async function getItemListFromServer(memberId){
+async function getItemListFromServer(memberId) {
     try {
-        const url = "/storage/item-list/"+memberId
+        const url = "/storage/item-list/" + memberId
         const resp = await fetch(url)
         const result = await resp.json();
         return result;
