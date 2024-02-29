67e5623759080f22c5a881d8b11e88a04dd87423
taehona6 <taehona6@gmail.com>
2024-02-29 20:50:53 +0900

    카드깡 애니메이션

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,3 +1,18 @@
 /**
  * 
  */
+
+function start() {
+    document.querySelector('.img-box').innerHTML=`
+    <img class="hd" src="/image/hd.png">
+    <div class="pokemon-card">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/493.png">
+        
+    </div>
+    <img class="bd" src="/image/bd.png">
+    `
+}
+
+function stop(){
+    document.querySelector('.img-box').innerHTML=``
+}
