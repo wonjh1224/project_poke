21a145a357b2bead39b3283ebb5feca4549c0a24
taehona6 <taehona6@gmail.com>
2024-03-01 03:23:33 +0900

    5개시작

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -16,6 +16,23 @@ function start() {
     <img class="bd" src="/image/bd.png">
     `
 }
+function start() {
+    document.querySelector('.img-box').innerHTML=`
+    <img class="hd" src="/image/hd.png">
+    <div class="pokemon-card card-anime">
+        <div>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/493.png">  
+        </div>
+        <p class="nes-badge grade">
+        <span class="is-warning">전설</span>
+        </p>
+        <p class="nes-badge">
+        <span class="is-dark">아르세우스</span>
+        </p>
+    </div>
+    <img class="bd" src="/image/bd.png">
+    `
+}
 
 function stop(){
     document.querySelector('.img-box').innerHTML=``
