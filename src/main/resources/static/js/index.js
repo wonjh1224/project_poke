dd7c37bd30e3c9dd13638751d37132e6cb457a29
taehona6 <taehona6@gmail.com>
2024-03-01 03:07:24 +0900

    카드깡임시저장

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,13 +1,17 @@
-/**
- * 
- */
 
 function start() {
     document.querySelector('.img-box').innerHTML=`
     <img class="hd" src="/image/hd.png">
-    <div class="pokemon-card">
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/493.png">
-        
+    <div class="pokemon-card card-anime">
+        <div>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/493.png">  
+        </div>
+        <p class="nes-badge grade">
+        <span class="is-warning">전설</span>
+        </p>
+        <p class="nes-badge">
+        <span class="is-dark">아르세우스</span>
+        </p>
     </div>
     <img class="bd" src="/image/bd.png">
     `
