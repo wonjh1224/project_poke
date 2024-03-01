e65851a0f52845450eb176cff043ca62ca21ed45
taehona6 <taehona6@gmail.com>
2024-03-02 02:17:09 +0900

    등급별 색상 다르게 했음

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -9,7 +9,7 @@ function start() {
     </div>
     <div class="card-pack">
         <img class="pack-head" src="/image/hd.png">
-        <div class="pokemon-card card-anime-color-legend">
+        <div class="pokemon-card card-anime-color-legendary">
             <div>
                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/493.png">  
             </div>
@@ -22,7 +22,67 @@ function start() {
         </div>
         <img class="pack-body" src="/image/bd.png">
     </div>
-       
+    <div class="card-pack">
+        <img class="pack-head" src="/image/hd.png">
+        <div class="pokemon-card card-anime-color-rare">
+            <div>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/493.png">  
+            </div>
+            <p class="nes-badge grade">
+            <span class="is-warning">전설</span>
+            </p>
+            <p class="nes-badge">
+            <span class="is-dark">아르세우스</span>
+            </p>
+        </div>
+        <img class="pack-body" src="/image/bd.png">
+    </div>
+    <div class="card-pack">
+        <img class="pack-head" src="/image/hd.png">
+        <div class="pokemon-card card-anime-color-uncommon">
+            <div>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/493.png">  
+            </div>
+            <p class="nes-badge grade">
+            <span class="is-warning">전설</span>
+            </p>
+            <p class="nes-badge">
+            <span class="is-dark">아르세우스</span>
+            </p>
+        </div>
+        <img class="pack-body" src="/image/bd.png">
+    </div>
+    <div class="card-pack">
+        <img class="pack-head" src="/image/hd.png">
+        <div class="pokemon-card card-anime-color-rare">
+            <div>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/493.png">  
+            </div>
+            <p class="nes-badge grade">
+            <span class="is-warning">전설</span>
+            </p>
+            <p class="nes-badge">
+            <span class="is-dark">아르세우스</span>
+            </p>
+        </div>
+        <img class="pack-body" src="/image/bd.png">
+    </div>
+    <div class="card-pack">
+        <img class="pack-head" src="/image/hd.png">
+        <div class="pokemon-card card-anime-color-uncommon">
+            <div>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/493.png">  
+            </div>
+            <p class="nes-badge grade">
+            <span class="is-warning">전설</span>
+            </p>
+            <p class="nes-badge">
+            <span class="is-dark">아르세우스</span>
+            </p>
+        </div>
+        <img class="pack-body" src="/image/bd.png">
+    </div>
+    
     `
 }
 
